29000a7d4c13d33783f02d954f3ca5e2aaeaaff2
Diego Salgado <Djsalgado14@gmail.com>
2021-04-06 10:24:36 -0700

    select form working

--- webapp.py
+++ webapp.py
@@ -8,16 +8,19 @@ def render_main():
     return render_template('home_page.html')
 @app.route("/big_money")
 def render_dog():
-    return render_template('wealth.html')
+    return render_template('wealth.html',bill=wealth_ammount())
 
 def wealth_ammount():
     with open('billionaires.json') as f:
         data = json.load(f)
-    listOfStates = []
+    amountOfmoney = []
+    options=""
     for x in data:
-        if data['State'] in listOfStates:
-            listOfStates.append('State')
+        if not x['wealth']['worth in billions'] in amountOfmoney:
+            amountOfmoney.append(x['wealth']['worth in billions'])
     print("data")
-
+    for number in amountOfmoney:
+        options += Markup("<option value=\"" + str(number) + "\">" + str(number) + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+    return options
 if __name__ =="__main__":
     app.run(debug=False,port=54321)
